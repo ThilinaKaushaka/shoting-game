8d52887e0e39f2434c68b725a024d7be1fc9d07f
ThilinaKaushaka <thilinaw72@gmail.com>
2025-03-18 00:43:52 +0530

    shooting and distroy enemy ship

--- src/javaScript/Scene2.js
+++ src/javaScript/Scene2.js
@@ -11,6 +11,11 @@ class Scene2 extends Phaser.Scene{
         this.ship2=this.add.sprite(config.width/2 , config.height/2, "ship2");
         this.ship3=this.add.sprite(config.width/2+50 , config.height/2, "ship3");
 
+        this.enemies=this.physics.add.group();
+        this.enemies.add(this.ship1);
+        this.enemies.add(this.ship2);
+        this.enemies.add(this.ship3);
+
         //this.ship1.setScale(2);
         //this.ship1.flipY=false;
         //this.ship1.angle+=3;
@@ -59,7 +64,32 @@ class Scene2 extends Phaser.Scene{
 
         this.spacebar=this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         this.projectiles=this.add.group();
+
+        this.physics.add.collider(this.projectiles,this.powerUps,function(projectile,powerUp){
+            projectile.destroy();
+        });
+
+        this.physics.add.overlap(this.player,this.powerUps,this.pickPowerUp,null,this);
+        this.physics.add.overlap(this.player,this.enemies,this.hurtPlayer,null,this);
+
+        this.physics.add.overlap(this.projectiles,this.enemies,this.hitEnemy,null,this);
     }
+
+    pickPowerUp(player,powerUp){
+        powerUp.disableBody(true,true);
+    }
+
+    hurtPlayer(player,enemy){
+        this.resetShipPosition(enemy);
+        player.x=config.width / 2-8;
+        player.y=config.height-64;
+    }
+
+    hitEnemy(projectile,enemy){
+        projectile.destroy();
+        this.resetShipPosition(enemy);
+    }
+
 
     update(){
         this.moveShip(this.ship1,1);
